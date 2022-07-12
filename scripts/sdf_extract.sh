d568de9b88dedc3e574e2cd907ce69512a535001
ChenXi <1515342618@qq.com>
2022-07-12 16:10:38 +0800

    change scripts according to the usage in readme

--- scripts/sdf_extract.sh
+++ scripts/sdf_extract.sh
@@ -6,12 +6,13 @@ now=$(date +"%Y%m%d_%H%M%S")
 jobname="sdf-$1-$now"
 echo "job name is $jobname"
 
-config_file="./config/train.yaml"
-ckpt_path=$2
+config_file=$2
+ckpt_path=$3
+eval_level=$4
 
 python -m torch.distributed.launch --nproc_per_node=4  tools/extract_mesh.py \
 --cfg_path ${config_file} \
 --mesh_size 1024 --chunk 102144 \
 --ckpt_path $ckpt_path \
---mesh_radius 1 --mesh_origin "0, 0, 0" --chunk_rgb 1024 --vertex_color --eval_level 10 \
+--mesh_radius 1 --mesh_origin "0, 0, 0" --chunk_rgb 1024 --vertex_color --eval_level ${eval_level} \
 2>&1|tee log/${jobname}.log
--- scripts/train.sh
+++ scripts/train.sh
@@ -7,14 +7,13 @@ now=$(date +"%Y%m%d_%H%M%S")
 jobname="train-$1-$now"
 echo "job name is $jobname"
 
-WORLD_SIZE=$2
-config_file="./config/train.yaml"
+config_file=$2
 mkdir -p log
 mkdir -p logs/${jobname}
 cp ${config_file} logs/${jobname}
 
 CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
 python train.py --cfg_path ${config_file} \
-  --num_gpus $2 --num_nodes $3 \
+  --num_gpus $3 --num_nodes $4 \
   --num_epochs 20 --batch_size 2048 --test_batch_size 512 --num_workers 16 \
   --exp_name ${jobname} 2>&1|tee log/${jobname}.log \
